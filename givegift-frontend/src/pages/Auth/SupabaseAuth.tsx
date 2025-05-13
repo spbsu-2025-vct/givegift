076a9d0e9ad666c6a8de0d4e8ed3f9a6d645298c
AmEl <amirelkanov0504@gmail.com>
2025-05-13 21:15:08 +0300

    Auth page

--- givegift-frontend/src/pages/Auth/SupabaseAuth.tsx
+++ givegift-frontend/src/pages/Auth/SupabaseAuth.tsx
@@ -2,31 +2,31 @@ import { Box } from "@mui/material";
 import { ThemeSupa } from "@supabase/auth-ui-shared";
 import { Auth } from "@supabase/auth-ui-react";
 import { useSupabase } from "../../context/SupabaseContext/SupabaseContext";
+import "./SupabaseAuth.css";
+import { ru } from "./locale";
+
 
 export const SupabaseAuth = () => {
     const { supabase } = useSupabase();
 
     return (
-        <Box
-            sx={{
-                margin: "15vh auto 0",
-                padding: "20px",
-                maxWidth: "30vw",
-                minWidth: "320px",
-                border: "1px solid lightgrey",
-                borderRadius: "4px",
-            }}
-        >
-            <Auth
-                supabaseClient={supabase}
-                providers={["google", "github"]}
-                appearance={{
-                    theme: ThemeSupa,
-                    className: {
-                        button: "custom-auth-button",
-                    },
-                }}
-            />
+        <Box className="auth-container">
+            <Box className="auth-wrapper slider">
+                <Auth
+                    supabaseClient={supabase}
+                    providers={["google"]}
+                    appearance={{
+                        theme: ThemeSupa,
+                        variables: {
+                            default: {
+                                colors: { brand: "#ff6332", brandAccent: "#ff6332" },
+                                fonts: { bodyFontFamily: "Montserrat, sans-serif" },
+                            },
+                        },
+                    }}
+                    localization={{ variables: ru }}
+                />
+            </Box>
         </Box>
     );
-}
+};
