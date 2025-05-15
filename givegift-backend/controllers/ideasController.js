1d429199fc80cccda0bb8be94227ad55a73c2984
AmEl <amirelkanov0504@gmail.com>
2025-05-15 22:36:25 +0300

    GENERATION WORKS!!! (but still need to think about imgs)

--- givegift-backend/controllers/ideasController.js
+++ givegift-backend/controllers/ideasController.js
@@ -1,19 +1,27 @@
 import { generateSystemPrompt, generateUserPrompt } from "../utils/prompts.js";
 import FireworksAIService from "../API/FireworksAIService.js";
+import { getProductInfo } from "../utils/WBParser.js";
 
 class IdeasController {
     async generateIdeas(req, res) {
         try {
+            const { price_range: priceRange, is_adult: isAdult, interests } = req.body;
+
             const rawResponse = await FireworksAIService.createChatCompletion(
                 generateSystemPrompt(),
-                generateUserPrompt(req.body.interests)
+                generateUserPrompt(interests)
             )
             const responseData = await rawResponse.json();
             const response = responseData.choices[0].message.content
                 .replace(/<think>[\s\S]*?<\/think>/g, "");
             const gifts = JSON.parse(response).gifts;
 
-            res.json(gifts)
+            const produtsPromises = gifts.map(gift =>
+                getProductInfo(gift, priceRange[0], priceRange[1], isAdult)
+            );
+
+            const data = await Promise.all(produtsPromises);
+            res.json(data)
         } catch (err) {
             console.error(err)
         }
--- givegift-backend/utils/WBParser.js
+++ givegift-backend/utils/WBParser.js
@@ -1,1 +1,91 @@
-// TODO: Parser
+// Build the Wildberries search URL for a given product and price range
+function getSearchLink(nameOfProduct, minBudget, maxBudget) {
+    const encodedName = encodeURIComponent(nameOfProduct);
+    return [
+        'https://search.wb.ru/exactmatch/ru/common/v4/search?appType=1',
+        `&curr=rub&dest=-1257786`,
+        `&priceU=${minBudget}00;${maxBudget}00`,
+        `&page=1&query=${encodedName}`,
+        `&resultset=catalog&sort=popular&spp=24`,
+        `&suppressSpellcheck=false`
+    ].join('');
+}
+
+// Fetch the search results JSON from Wildberries
+async function getQueryData(queryLink) {
+    const rawResponse = await fetch(queryLink);
+    return await rawResponse.json();
+}
+
+// Extract the first product ID & name from the response
+function getFirstProductIdAndName(response, adult = false) {
+    try {
+        const products = response?.data?.products;
+
+        if (!Array.isArray(products) || products.length === 0) return null;
+
+        const product = adult ? products[0] : (products.find(p => !p.isAdult) || null)
+        return [product?.id, product?.name]
+    } catch {
+        return null;
+    }
+}
+
+
+// TODO: their way to store images has changed. Need to look through
+// Build the image URL for a given product ID
+function getImageLink(productId) {
+    try {
+        const shortId = Math.floor(productId / 100000);
+        // Determine basket based on shortId range
+        let basket;
+        if (shortId <= 143) basket = '01';
+        else if (shortId <= 287) basket = '02';
+        else if (shortId <= 431) basket = '03';
+        else if (shortId <= 719) basket = '04';
+        else if (shortId <= 1007) basket = '05';
+        else if (shortId <= 1061) basket = '06';
+        else if (shortId <= 1115) basket = '07';
+        else if (shortId <= 1169) basket = '08';
+        else if (shortId <= 1313) basket = '09';
+        else if (shortId <= 1601) basket = '10';
+        else if (shortId <= 1655) basket = '11';
+        else if (shortId <= 1919) basket = '12';
+        else basket = '13';
+
+        const vol = shortId;
+        const part = Math.floor(productId / 1000);
+        return `https://basket-${basket}.wb.ru/vol${vol}/part${part}/${productId}/images/big/1.jpg`;
+    } catch {
+        return null;
+    }
+}
+
+/* interface IProduct {
+    market_link: string;
+    title: string;
+    img_link: string
+} */
+async function getProductInfo(productName, minBudget, maxBudget, adult = false) {
+    try {
+        const productQueryLink = getSearchLink(productName, minBudget, maxBudget);
+        const response = await getQueryData(productQueryLink);
+        const productIDName = getFirstProductIdAndName(response, adult);
+        if (!productIDName) return null;
+
+        const [id, title] = productIDName;
+        const imgLink = getImageLink(id);
+
+        return {
+            market_link: productQueryLink,
+            title,
+            img_link: imgLink
+        }
+    } catch (err) {
+        console.error(err)
+        return null;
+    }
+}
+
+
+export { getProductInfo };
